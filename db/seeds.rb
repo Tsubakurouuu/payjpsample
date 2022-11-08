ce8d65a562a83f258e44bef2a8f2db469e60b96f
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-08 03:05:31 +0000

    productsモデル、コントローラの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = [
+  { name: '鉛筆', price: 100 },
+  { name: 'ボールペン', price: 200 },
+  { name: '消しゴム', price: 50 },
+  { name: 'ノート', price: 100 }
+]
+Product.create(products)

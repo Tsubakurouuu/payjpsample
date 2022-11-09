f2fbc4a5972b5d50e84844f2fe346cc31f6299fa
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-09 04:34:32 +0000

    [Fix]application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "card.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-require("@rails/ujs").start()
-// require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("../card")
